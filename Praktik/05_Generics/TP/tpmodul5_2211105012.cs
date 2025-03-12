dc1821722d4c702b3a1847e9b2ca3ad70e4f1116
nadiaputrirah <nadiapputriirah@gmail.com>
2025-03-12 23:55:19 +0700

    menambahkan class DataGeneric

---
+++ Praktik/05_Generics/TP/tpmodul5_2211105012.cs
@@ -0,0 +1,29 @@
+﻿class HaloGeneric {
+    public void SapaUser<T>(T user) {
+        Console.WriteLine($"Halo user {user}");
+    }
+}
+
+class DataGeneric<T> {
+    public T Data { get; set; }
+
+    public DataGeneric(T data) {
+        Data = data;
+    }
+
+    public void PrintData() {
+        Console.WriteLine($"Data yang tersimpan adalah: {Data}");
+    }
+}
+
+class tpmodul5_2211104012
+{
+    static void Main() {
+        HaloGeneric halo = new HaloGeneric();
+        halo.SapaUser("Praktikan");
+
+        // Menyimpan NIM dalam DataGeneric dan mencetaknya
+        DataGeneric<string> nimData = new DataGeneric<string>("2211104012");
+        nimData.PrintData();
+    }
+}
