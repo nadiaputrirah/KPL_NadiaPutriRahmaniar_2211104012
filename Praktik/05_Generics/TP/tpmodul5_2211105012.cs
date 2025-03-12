d6a46bc3d26f16fa9b0e8c8ce738807b5488e910
nadiaputrirah <nadiapputriirah@gmail.com>
2025-03-12 23:49:19 +0700

    menambahkan class HaloGeneric dengan method SapaUser

---
+++ Praktik/05_Generics/TP/tpmodul5_2211105012.cs
@@ -0,0 +1,16 @@
+﻿class HaloGeneric
+{
+    public void SapaUser<T>(T user)
+    {
+        Console.WriteLine($"Halo user {user}");
+    }
+}
+
+class tpmodul5_2211104012
+{
+    static void Main()
+    {
+        HaloGeneric halo = new HaloGeneric();
+        halo.SapaUser("Nadia Putri Rahmaniar");
+    }
+}
