4e19ec482d4ccfd4f0937c4409352942644960df
nadiaputrirah <nadiapputriirah@gmail.com>
2025-03-20 14:05:00 +0700

    menambahkan class KuliahMahasiswa2211104012

---
+++ Praktik/07_Grammar-Based_Input_Processing_Parsing/KuliahMahasiswa2211104012.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.IO;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+
+class KuliahMahasiswa2211104012
+{
+    public class Course
+    {
+        public string Code { get; set; }
+        public string Name { get; set; }
+    }
+
+    public class CourseList
+    {
+        public List<Course> Courses { get; set; }
+    }
+
+    public static void ReadJSON()
+    {
+        string filePath = "tp7_2_2211104012.json";
+
+        // Cek apakah file JSON ada
+        if (!File.Exists(filePath))
+        {
+            Console.WriteLine("File JSON tidak ditemukan.");
+            return;
+        }
+
+        // Membaca isi file JSON
+        string jsonContent = File.ReadAllText(filePath);
+
+        // Melakukan deserialisasi JSON ke objek
+        CourseList data = JsonConvert.DeserializeObject<CourseList>(jsonContent);
+
+        // Menampilkan hasil
+        Console.WriteLine("Daftar mata kuliah yang diambil:");
+        int index = 1;
+        foreach (var course in data.Courses)
+        {
+            Console.WriteLine($"MK {index} {course.Code} - {course.Name}");
+            index++;
+        }
+    }
+}
--- Praktik/07_Grammar-Based_Input_Processing_Parsing/Program.cs
+++ Praktik/07_Grammar-Based_Input_Processing_Parsing/Program.cs
@@ -5,6 +5,7 @@ class Program
     static void Main()
     {
         DataMahasiswa2211104012.ReadJSON();
+        KuliahMahasiswa2211104012.ReadJSON();
     }
 }
 
