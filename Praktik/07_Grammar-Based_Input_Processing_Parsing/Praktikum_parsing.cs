4aa41c2f753357385e7a765aa7e29420594eff14
nadiaputrirah <nadiapputriirah@gmail.com>
2025-04-18 12:39:26 +0700

    Update modul 7

---
+++ Praktik/07_Grammar-Based_Input_Processing_Parsing/Praktikum_parsing.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Net; 
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace praktikum_parsing.json
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            WebClient c = new WebClient();
+            var data = c.DownloadString("https://blockchain.info/ticker");
+            //
+            JObject currencies = JObject.Parse(data);
+            var currency = currencies.SelectToken("GBP");
+            var usdollar = currencies.SelectToken("USD");
+            Console.WriteLine("British Pounds: " + currency);
+            Console.WriteLine("US Dollar: " + usdollar);
+            Console.Read();
+        }
+    }
+}
---
+++ Praktik/07_Grammar-Based_Input_Processing_Parsing/Praktikum_parsing_string.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace praktikum_parsing.json
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string dateStr = "March 20, 2025 13:30:00";
+            DateTime tanggal = DateTime.Parse(dateStr);
+            Console.WriteLine($"Hasil Parsing DateTime dengan waktu: {tanggal}");
+        }
+    }
+}
